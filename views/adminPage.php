fc09bd545ce32225208eabfcd0ac8a3a541d2bf3
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-26 22:07:06 +0700

    feat: create view v1

---
+++ views/adminPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="admin-page">
+        <h1>Welcome, <span id="">"NAMA ADMIN"</span></h1>
+        <p>This is an <span>admin</span> page</p>
+        <br>
+        <input type="button" value="logout">
+    </div>
+</body>
+</html>
