39822538255663565a42bee6e6eefce557b20926
ditoaryap <ditoaryaputra1324@gmail.com>
2025-11-27 23:48:44 +0700

    feat: views user page

---
+++ views/userPage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="user-page">
+        <h1>Welcome, <span id="">"NAMA USER"</span></h1>
+        <p>This is an <span>user</span> page</p>
+        <br>
+        <input type="button" value="logout">
+    </div>
+</body>
+</html>
